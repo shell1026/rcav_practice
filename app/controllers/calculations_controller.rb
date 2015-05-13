cfc6dd25185490c7f2f8fb06ef2aa5e14211dbec
shell1026 <mretson0@chicagobooth.edu>
2015-05-13 00:30:17 -0500

    finished
    only thing not right is number of decimals for square root answer.

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -7,4 +7,25 @@ class CalculationsController < ApplicationController
 
   def pmt
   end
+
+  def square_number
+    @num = params[:num]
+    @square =@num.to_f**2
+  end
+
+  def square_root
+    @num = params[:num].to_f
+    @square_root = Math.sqrt(@num)
+  end
+
+  def loan_payment
+    @rate = params[:rate].to_f
+    @payments = params[:payments].to_f
+    @principal = params[:principal].to_f
+    rate = (@rate / 100 / 12).to_f
+
+    @monthly_payment = (rate*@principal)/(1-(1+rate)**-@payments)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/",                  { :controller => "calculations", :action => "instructions" })
+  get("/square/:num",       { :controller => "calculations", :action => "square_number"})
+  get("/square_root/:num", { :controller => "calculations", :action => "square_root"})
+  get("/payment/:rate/:payments/:principal", { :controller => "calculations", :action => "loan_payment"})
 end
